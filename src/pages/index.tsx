02710718a0e901c4da6c0b1aef5acd2b69d81b33
HecateAkke <hecate.akke@gmail.com>
2024-04-07 07:10:39 +0800

    Update translations and fix typos in documentation files

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -114,6 +114,22 @@ const otherLibraries = [
       </svg>
     ),
   },
+  {
+    content: "Redux的记忆选择器库",
+    title: "Reselect 中文",
+    link: "./reselect/introduction/getting-started",
+    image: (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        aria-hidden="true"
+        data-icon="external-link-square-alt"
+        data-prefix="fas"
+        viewBox="0 0 448 512"
+      >
+        <path d="M448 80v352c0 26.51-21.49 48-48 48H48c-26.51 0-48-21.49-48-48V80c0-26.51 21.49-48 48-48h352c26.51 0 48 21.49 48 48zm-88 16H248.029c-21.313 0-32.08 25.861-16.971 40.971l31.984 31.987L67.515 364.485c-4.686 4.686-4.686 12.284 0 16.971l31.029 31.029c4.687 4.686 12.285 4.686 16.971 0l195.526-195.526 31.988 31.991C358.058 263.977 384 253.425 384 231.979V120c0-13.255-10.745-24-24-24z"></path>
+      </svg>
+    ),
+  },
 ];
 
 function Home() {
@@ -185,7 +201,7 @@ function Home() {
                 {otherLibraries.map(({ image, title, content, link }, idx) => (
                   <div
                     key={idx}
-                    className={classnames("col col--6", styles.feature)}
+                    className={classnames("col col--4", styles.feature)}
                   >
                     <h2 className="text--center">
                       <a target="_blank" href={link} className={styles.featureAnchor}>
